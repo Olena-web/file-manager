b3d642337cc87a991e610299eb1d5cf8b6b9ef0c
Olena Honcharuk <gasmin07@gmail.com>
2022-12-19 22:02:41 +0200

    feat: add solution for already exist path in copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,12 +1,20 @@
+import * as fs from 'fs/promises';
 import { createReadStream, createWriteStream } from 'fs';
 import { EOL } from 'os';
-import { getAbsolutePath } from '../utils/doesExist.js';
+import { getAbsolutePath, doesExist } from '../utils/doesExist.js';
 import { currentDirMessage } from '../utils/currentDirMessage.js';
 
 export const copy = async (fileToCopy, newDestination, cwd) => {
     const absolutePath = getAbsolutePath(fileToCopy, cwd);
     const newAbsolutePath = getAbsolutePath(newDestination, cwd);
-    console.log(absolutePath, newAbsolutePath);
+    if (await fs.access
+        (newAbsolutePath)
+        .then(() => true)
+        .catch(() => false)) {
+        process.stdout.write(`${EOL}File ${newAbsolutePath} already exists.${EOL}`);
+        currentDirMessage(`${cwd}`);
+        return;
+    }
 
     try {
         const readable = createReadStream(absolutePath);
